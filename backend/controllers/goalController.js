5e8ea167204fdd65eb940418fa6d1fb46e1dd09e
Imon <contact@imonweb.co.uk>
2022-10-15 23:42:21 +0100

    app

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,10 @@
+// @desc Get goals
+// @route GET /api/goals
+// @access Private
+const getGoals = (req, res) => {
+  res.status(200).json({message: 'Get goals'})
+}
+
+module.exports = {
+  getGoals,
+}
